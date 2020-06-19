bae75c3fd0a48e2b25d9e6ffa2580b92c9c875ac
Abdul Akibu <aakibu.dev@gmail.com>
2020-06-19 17:23:15 -0400

    Updates

---
+++ precache-manifest.b1b466d481c8af54b1d97d1a1dabb127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dad81c51d45e5187a47da8d51ce3fee",
+    "url": "/Testimonial-Slider-React/index.html"
+  },
+  {
+    "revision": "796e9edbbe0ffc25a707",
+    "url": "/Testimonial-Slider-React/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "d28064629769f6c83d04",
+    "url": "/Testimonial-Slider-React/static/js/2.f4e0b740.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Testimonial-Slider-React/static/js/2.f4e0b740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796e9edbbe0ffc25a707",
+    "url": "/Testimonial-Slider-React/static/js/main.92254184.chunk.js"
+  },
+  {
+    "revision": "e9316b523cc433c70d9f",
+    "url": "/Testimonial-Slider-React/static/js/runtime-main.c9169bce.js"
+  }
+]);
---
+++ static/js/main.92254184.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfinal=this.webpackJsonpfinal||[]).push([[0],[,,,function(n,e,t){n.exports=t(10)},,,,,function(n,e,t){},function(n,e,t){},function(n,e,t){"use strict";t.r(e);var a=t(0),c=t.n(a),o=t(2),r=t.n(o);t(8);var i=function(){return c.a.createElement("div",{className:"App"},"App Component")};t(9);r.a.render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(i,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.92254184.chunk.js.map
